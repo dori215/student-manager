357f101d47460c3d0c9c3a64230ee90ba0235d61
alescsc <76198547+alescsc@users.noreply.github.com>
2026-04-01 03:45:29 +0300

    SM-2 Added the students Table and some dummy student list

--- src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
+++ src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
@@ -4,16 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+        primaryStage.setTitle("Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.example.studentmanager {
 
 
     opens loose.oose.fis.lab.student.manager to javafx.fxml;
+    opens loose.oose.fis.lab.student.manager.model to javafx.base;
+    opens loose.oose.fis.lab.student.manager.controllers to javafx.fxml;
     exports loose.oose.fis.lab.student.manager;
 }
